ccbbea22f4700a5ffea940c4d58e7459a0ff6242
WellingtonVinii <wellingtonvinii10@gmail.com>
2024-12-06 13:05:27 -0300

    Finalização

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ function menu(req,resp){
         <body>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Menu Principal</a>
+                <a class="navbar-brand">Menu Principal</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
                 <span class="navbar-toggler-icon"></span></button>
 
